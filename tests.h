292e08f109517a92a3ad86e501fe015c57f8665a
Artem <64967389+temontii@users.noreply.github.com>
2025-04-20 22:51:09 +0300

    Update tests.h

--- tests.h
+++ tests.h
@@ -3,16 +3,19 @@
 
 #include "Polynomial.h"
 
+typedef struct {
+    const char* description;
+    Polynomial* (*setup)();
+    void (*test_func)(Polynomial*);
+    void (*cleanup)(Polynomial*);
+} PolynomialTest;
+
 void run_all_tests();
+void print_test_details(const char* operation, const Polynomial* expected, const Polynomial* actual, PolynomialError status);
 void test_int_poly_creation();
-void test_complex_poly_addition();
-void test_poly_multiplication();
-void test_scalar_multiplication();
-void test_poly_evaluation();
 void test_zero_and_minimal_polynomials();
 void test_large_numbers_and_high_degrees();
 void test_diff_size_polynomials();
-void test_error_handling();
 
 
 #endif
